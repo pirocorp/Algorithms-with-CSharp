025f87d953c1267ffc87f2d8641f7830a6e239b4
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-09 18:03:12 +0100

    05. Combinations without Repetition

---
+++ 01. RECURSION/Exercise/05. Combinations without Repetition/CombinationsWithoutRepetitionProgram.cs
@@ -0,0 +1,38 @@
+﻿namespace _05._Combinations_without_Repetition
+{
+    using System;
+
+    public static class CombinationsWithoutRepetitionProgram
+    {
+        public static void Main()
+        {
+            var set = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            var vector = new int[k];
+
+            GetCombos(set, vector, 0, 1);
+        }
+
+        private static void GetCombos(int set, int[] vector, int index, int border)
+        {
+            if (index >= vector.Length)
+            {
+                Print(vector);
+            }
+            else
+            {
+                for (var i = border; i <= set; i++)
+                {
+                    vector[index] = i;
+                    GetCombos(set, vector, index + 1, i + 1);
+                }
+            }
+        }
+
+        private static void Print(int[] vector)
+        {
+            Console.WriteLine(string.Join(" ", vector));
+        }
+    }
+}
