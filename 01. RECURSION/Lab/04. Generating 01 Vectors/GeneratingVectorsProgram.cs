5cb0370196b84c17bffae969a16e79c2fc21ff84
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-09 16:27:01 +0100

    04. Generating 01 Vectors

---
+++ 01. RECURSION/Lab/04. Generating 01 Vectors/GeneratingVectorsProgram.cs
@@ -0,0 +1,44 @@
+﻿namespace _04._Generating_01_Vectors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class GeneratingVectorsProgram
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var result = GetVectors(n)
+                .Select(x => string.Join("", x));
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static List<byte[]> GetVectors(int i)
+        {
+            var vectors = new List<byte[]>();
+
+            GenerateVectors(new byte[i], 0, vectors);
+
+            return vectors;
+        }
+
+        private static void GenerateVectors(byte[] vector, int index, List<byte[]> vectors)
+        {
+            if (index == vector.Length)
+            {
+                var clone = new byte[vector.Length];
+                Array.Copy(vector, clone, vector.Length);
+                vectors.Add(clone);
+                return;
+            }
+
+            for (byte i = 0; i <= 1; i++)
+            {
+                vector[index] = i;
+                GenerateVectors(vector, index + 1, vectors);
+            }
+        }
+    }
+}
