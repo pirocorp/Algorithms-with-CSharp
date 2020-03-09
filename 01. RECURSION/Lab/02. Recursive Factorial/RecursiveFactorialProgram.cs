1f956a293ddfee0e72e51cee67facd374c4f0820
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-09 16:26:35 +0100

    02. Recursive Factorial

---
+++ 01. RECURSION/Lab/02. Recursive Factorial/RecursiveFactorialProgram.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._Recursive_Factorial
+{
+    using System;
+
+    public static class RecursiveFactorialProgram
+    {
+        public static void Main()
+        {
+            var factor = int.Parse(Console.ReadLine());
+            var result = Factor(factor);
+            Console.WriteLine(result);
+        }
+
+        private static long Factor(int factor)
+        {
+            if (factor == 1)
+            {
+                return 1;
+            }
+
+            return factor * Factor(factor - 1);
+        }
+    }
+}
