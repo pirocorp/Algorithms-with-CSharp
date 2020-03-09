737c24356de94fcc9c710348c174429dda297124
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-09 18:02:40 +0100

    01. Reverse Array

---
+++ 01. RECURSION/Exercise/01. Reverse Array/ReverseArrayProgram.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Reverse_Array
+{
+    using System;
+    using System.Linq;
+
+    public static class ReverseArrayProgram
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            ReverseArray(0, numbers);
+        }
+
+        private static void ReverseArray(int index, int[] numbers)
+        {
+            if (index == numbers.Length)
+            {
+                return;
+            }
+
+            ReverseArray(index + 1, numbers);
+            Console.Write($"{numbers[index]} ");
+        }
+    }
+}
