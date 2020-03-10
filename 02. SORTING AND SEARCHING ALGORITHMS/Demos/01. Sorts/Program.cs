db13c53792b62ace09045fe3c11b9f1b63a8e3ca
Zdravko Zdravkov <pirocorp@abv.bg>
2020-03-10 17:30:16 +0100

    Added SelectionSort Demo

---
+++ 02. SORTING AND SEARCHING ALGORITHMS/Demos/01. Sorts/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Sorts
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var collection = new List<int>()
+            {
+                3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48
+            };
+
+            //Sorts<int>.SelectionSort(collection);
+
+            Console.WriteLine(string.Join(", ", collection));
+        }
+    }
+}
---
+++ 02. SORTING AND SEARCHING ALGORITHMS/Demos/01. Sorts/Sorts.cs
@@ -0,0 +1,44 @@
+﻿namespace _01._Sorts
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Sorts<T> where T : IComparable
+    {
+        public static void SelectionSort(IList<T> collection)
+        {
+            //Worst Case: n^2, Average Case: n^2, Best Case: n^2
+            for (var currentIndex = 0; currentIndex < collection.Count; currentIndex++)
+            {
+                var min = collection[currentIndex];
+                var swap = -1;
+
+                for (var swapIndex = currentIndex + 1; swapIndex < collection.Count; swapIndex++)
+                {
+                    if (ReOrder(min, collection[swapIndex]))
+                    {
+                        swap = swapIndex;
+                        min = collection[swapIndex];
+                    }
+                }
+
+                if (swap != -1)
+                {
+                    Swap(collection, currentIndex, swap);
+                }
+            }
+        }
+
+        private static void Swap(IList<T> collection, int from, int to)
+        {
+            var swap = collection[from];
+            collection[from] = collection[to];
+            collection[to] = swap;
+        }
+
+        private static bool ReOrder(IComparable first, IComparable second)
+        {
+            return first.CompareTo(second) > 0;
+        }
+    }
+}
