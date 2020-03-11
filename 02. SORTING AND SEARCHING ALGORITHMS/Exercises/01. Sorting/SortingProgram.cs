b2e2f8fcb06143e20724af4abccc3d3b69320bd3
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-11 12:01:12 +0100

    01. Sorting

---
+++ 02. SORTING AND SEARCHING ALGORITHMS/Exercises/01. Sorting/SortingProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Sorting
+{
+    using System;
+    using System.Linq;
+
+    public static class SortingProgram
+    {
+        public static void Main()
+        {
+            var inputArr = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            MergeSort<int>.Sort(inputArr);
+            Console.WriteLine(string.Join(" ", inputArr));
+        }
+    }
+}
