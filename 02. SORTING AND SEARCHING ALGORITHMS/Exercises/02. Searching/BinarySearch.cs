d50eb0e463c40f6f5daf8166201636971b867f48
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-11 12:01:24 +0100

    02. Searching

---
+++ 02. SORTING AND SEARCHING ALGORITHMS/Exercises/02. Searching/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace _02._Searching
+{
+    public static class BinarySearch
+    {
+        public static int IndexOf(int[] arr, int key)
+        {
+            var lo = 0;
+            var hi = arr.Length - 1;
+
+            while (lo <= hi)
+            {
+                var mid = lo + (hi - lo) / 2;
+                if (key < arr[mid])
+                {
+                    hi = mid - 1;
+                }
+                else if (key > arr[mid])
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 02. SORTING AND SEARCHING ALGORITHMS/Exercises/02. Searching/SearchingProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace _02._Searching
+{
+    using System;
+    using System.Linq;
+
+    public static class SearchingProgram
+    {
+        public static void Main()
+        {
+            var inputArr = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var key = int.Parse(Console.ReadLine());
+
+            var index = BinarySearch.IndexOf(inputArr, key);
+            Console.WriteLine(index);
+        }
+    }
+}
