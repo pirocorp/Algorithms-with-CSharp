960f6e4ae9dd925e0d026d0abc84921c090c9bc9
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-11 11:48:10 +0100

    03. Binary Search

---
+++ 02. SORTING AND SEARCHING ALGORITHMS/Lab/03. Binary Search/BinarySearchProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace _03._Binary_Search
+{
+    using System;
+
+    public static class BinarySearchProgram
+    {
+        public static void Main()
+        {
+            var arr = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 105, 106, 200};
+            var index = BinarySearch.IndexOf(arr, 105);
+
+            Console.WriteLine(arr[index]);
+        }
+    }
+}
