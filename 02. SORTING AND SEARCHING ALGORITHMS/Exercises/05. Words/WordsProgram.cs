6cd037bf59ce17764128a9ea61a6fe8f56d02edf
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-11 16:31:28 +0100

    05. Words

--- 02. SORTING AND SEARCHING ALGORITHMS/Exercises/05. Words/WordsProgram.cs
+++ 02. SORTING AND SEARCHING ALGORITHMS/Exercises/05. Words/WordsProgram.cs
@@ -1,9 +1,69 @@
 ﻿namespace _05._Words
 {
+    using System;
+    using System.Collections.Generic;
+
     public static class WordsProgram
     {
+        private static int _count;
+        private static char[] _symbols;
+
         public static void Main()
         {
+            _symbols = Console.ReadLine()?.ToCharArray();
+
+            GeneratePermutations(0);
+
+            Console.WriteLine(_count);
+        }
+
+        private static void GeneratePermutations(int index)
+        {
+            if (index == _symbols.Length)
+            {
+                var isValid = true;
+
+                for (var i = 0; i < _symbols.Length - 1; i++)
+                {
+                    var current = _symbols[i];
+                    var next = _symbols[i + 1];
+
+                    if (current == next)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid)
+                {
+                    _count++;
+                }
+
+                return;
+            }
+
+            var swapped = new HashSet<char>();
+
+            for (var i = index; i < _symbols.Length; i++)
+            {
+                if (!swapped.Contains(_symbols[i]))
+                {
+                    Swap(index, i);
+
+                    GeneratePermutations(index + 1);
+
+                    swapped.Add(_symbols[index]);
+                    Swap(index, i);
+                }
+            }
+        }
+
+        private static void Swap(int from, int to)
+        {
+            var temp = _symbols[from];
+            _symbols[from] = _symbols[to];
+            _symbols[to] = temp;
         }
     }
 }
