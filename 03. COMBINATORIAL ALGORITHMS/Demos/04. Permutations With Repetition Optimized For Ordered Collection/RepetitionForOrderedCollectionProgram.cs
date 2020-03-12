a57ee096ba1b84039e6107ee33313c47a5c937a8
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-12 14:21:03 +0100

    04. Permutations With Repetition Optimized For Ordered Collection

---
+++ 03. COMBINATORIAL ALGORITHMS/Demos/04. Permutations With Repetition Optimized For Ordered Collection/RepetitionForOrderedCollectionProgram.cs
@@ -0,0 +1,52 @@
+﻿namespace _04._Permutations_With_Repetition_Optimized_For_Ordered_Collection
+{
+    using System;
+
+    public static class RepetitionForOrderedCollectionProgram
+    {
+        public static void Main()
+        {
+            var arr = new int[] { 3, 5, 1, 5, 5 };
+            Array.Sort(arr);
+            PermuteRep(arr, 0, arr.Length - 1);
+        }
+
+        private static void PermuteRep(int[] arr, int start, int end)
+        {
+            Print(arr);
+
+            for (var left = end - 1; left >= start; left--)
+            {
+                for (var right = left + 1; right <= end; right++)
+                {
+                    if (arr[left] != arr[right])
+                    {
+                        Swap(ref arr[left], ref arr[right]);
+                        PermuteRep(arr, left + 1, end);
+                    }
+                }
+
+                var firstElement = arr[left];
+
+                for (var i = left; i < end; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+
+                arr[end] = firstElement;
+            }
+        }
+
+        private static void Swap(ref int left, ref int right)
+        {
+            var swap = left;
+            left = right;
+            right = swap;
+        }
+
+        private static void Print(int[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
