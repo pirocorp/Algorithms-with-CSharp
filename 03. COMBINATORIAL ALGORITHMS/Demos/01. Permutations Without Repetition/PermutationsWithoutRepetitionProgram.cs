b24d0be3689ba4268ac70ab6e5b3228bdc7d722e
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-12 14:19:52 +0100

    01. Permutations Without Repetition

---
+++ 03. COMBINATORIAL ALGORITHMS/Demos/01. Permutations Without Repetition/PermutationsWithoutRepetitionProgram.cs
@@ -0,0 +1,45 @@
+﻿namespace _01._Permutations_Without_Repetition
+{
+    using System;
+
+    public static class PermutationsWithoutRepetitionProgram
+    {
+        private static int[] _elements;
+        private static bool[] _used;
+        private static int[] _permutation;
+
+        //Count of permutation is n!
+        private static void Permute(int currentCellIndex)
+        {
+            if (currentCellIndex == _permutation.Length)
+            {
+                Console.WriteLine(string.Join(" ", _permutation));
+            }
+            else
+            {
+                for (var i = 0; i < _elements.Length; i++)
+                {
+                    if (!_used[i])
+                    {
+                        var currentElement = _elements[i];
+                        _used[i] = true;
+
+                        _permutation[currentCellIndex] = currentElement;
+                        Permute(currentCellIndex + 1); 
+
+                        _used[i] = false;
+                    }
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            _elements = new[] {1, 2, 3};
+            _used = new bool[_elements.Length];
+            _permutation = new int[_elements.Length];
+
+            Permute(0);
+        }
+    }
+}
