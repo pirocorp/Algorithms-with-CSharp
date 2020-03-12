e6c6ca273ccd956b6948bbe33ca32b75df2d4d23
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-12 14:21:53 +0100

    08. Combinations Without Repetition

---
+++ 03. COMBINATORIAL ALGORITHMS/Demos/08. Combinations Without Repetition/CombinationsWithoutRepetitionProgram.cs
@@ -0,0 +1,40 @@
+﻿namespace _08._Combinations_Without_Repetition
+{
+    using System;
+
+    public static class CombinationsWithoutRepetitionProgram
+    {
+        private static int[] _elements;
+        private static int[] _combination;
+
+        public static void Main()
+        {
+            var combinationSize = 3;
+            _elements = new[] { 1, 2, 3, 4, 5 };
+            _combination = new int[combinationSize];
+            Combination(0, 0);
+        }
+
+        //Combination Count = n! / k!(n-k)!
+        private static void Combination(int index, int start)
+        {
+            if (index == _combination.Length)
+            {
+                Print();
+            }
+            else
+            {
+                for (var i = start; i < _elements.Length; i++)
+                {
+                    _combination[index] = _elements[i];
+                    Combination(index + 1, i + 1);
+                }
+            }
+        }
+
+        private static void Print()
+        {
+            Console.WriteLine(string.Join(" ", _combination));
+        }
+    }
+}
