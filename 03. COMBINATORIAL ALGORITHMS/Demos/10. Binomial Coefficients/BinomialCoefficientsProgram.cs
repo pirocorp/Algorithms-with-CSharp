48a3ce45ce720d7d2f4c224831b3cbecfc52cd03
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-12 14:22:15 +0100

    10. Binomial Coefficients

---
+++ 03. COMBINATORIAL ALGORITHMS/Demos/10. Binomial Coefficients/BinomialCoefficientsProgram.cs
@@ -0,0 +1,23 @@
+﻿namespace _10._Binomial_Coefficients
+{
+    using System;
+
+    public static class BinomialCoefficientsProgram
+    {
+        private static decimal Binom(int n, int k)
+        {
+            if (k == 0)
+            {
+                return 1;
+            }
+
+            //SLOW
+            return (n * Binom(n - 1, k - 1)) / k;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(Binom(49, 6));
+        }
+    }
+}
