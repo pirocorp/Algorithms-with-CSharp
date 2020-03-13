38b9b7bf1e9dd935ff5d9255a1501e4a10c90f8d
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-13 18:38:02 +0100

    Added structures

---
+++ Workshops/02. Lazy Recursion/LazyRecursion/EntryPoint.cs
@@ -0,0 +1,82 @@
+﻿namespace LazyRecursion
+{
+    using System;
+    using LazyTypes;
+    using Range = LazyTypes.Range;
+
+    public static class EntryPoint
+    {
+        public static void Main()
+        {
+            //DemoConcept();
+            //PairDemoConcept();
+            //OptionalDemoConcept();
+            RangeDemoConcept();
+        }
+
+        private static void RangeDemoConcept()
+        {
+            var start = new Lazy<int>(() => 1);
+            var end = new Lazy<int>(() => 10);
+            var list = Range.FromTo(start, end);
+
+
+        }
+
+        static Optional<int> Divide(Lazy<int> x, Lazy<int> y)
+        {
+            if (y.Value == 0)
+            {
+                return new Optional<int>();
+            }
+
+            return new Optional<int>(new Lazy<int>(() => x.Value / y.Value));
+        }
+
+        private static void OptionalDemoConcept()
+        {
+            var x = new Lazy<int>(() => 18);
+            var y = new Lazy<int>(() => 2);
+            var z = new Lazy<int>(() => 3);
+
+            var result = new Optional<int>(x)
+                .Bind(v => Divide(v, y))
+                .Bind(v => Divide(v, z))
+                .WithOptional(
+                    new Lazy<int>(() =>
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                        return 0;
+                    }),
+                    (v) => new Lazy<int>(() =>
+                    {
+                        Console.WriteLine($"Result is {v.Value}");
+                        return v.Value;
+                    }));
+
+            Console.WriteLine(result.Value);
+        }
+
+        private static void PairDemoConcept()
+        {
+            var x = new Lazy<int>(() => 4);
+            var y = new Lazy<int>(() => 5);
+
+            var pair = Pair.Make(x, y);
+            pair.WithPair<int>((x, y) =>
+            {
+                Console.WriteLine($"{x.Value} {y.Value}");
+                return new Lazy<int>(() => 0);
+            });
+        }
+
+        private static void DemoConcept()
+        {
+            var x = new Lazy<int>(() => int.Parse(Console.ReadLine()));
+            var y = new Lazy<int>(() => int.Parse(Console.ReadLine()));
+
+            var z = new Lazy<int>(() => x.Value + y.Value);
+            Console.WriteLine(z.Value);
+        }
+    }
+}
---
+++ Workshops/02. Lazy Recursion/LazyRecursion/Lazy.cs
@@ -0,0 +1,30 @@
+﻿//namespace LazyRecursion
+//{
+//    using System;
+
+//    public class Lazy<T> 
+//        where T : struct
+//    {
+//        private T? _value;
+//        private readonly Func<T> _func;
+
+//        public Lazy(Func<T> func)
+//        {
+//            this._value = null;
+//            this._func = func;
+//        }
+
+//        public T Value
+//        {
+//            get
+//            {
+//                if (!this._value.HasValue)
+//                {
+//                    this._value = this._func();
+//                }
+
+//                return this._value.Value;
+//            }
+//        }
+//    }
+//}
