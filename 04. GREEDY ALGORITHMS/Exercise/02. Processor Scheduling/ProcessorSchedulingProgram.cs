ffd647d371719d2e43f5004dcddb03361f64c551
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-17 14:52:38 +0100

    02. Processor Scheduling

---
+++ 04. GREEDY ALGORITHMS/Exercise/02. Processor Scheduling/ProcessorSchedulingProgram.cs
@@ -0,0 +1,68 @@
+﻿namespace _02._Processor_Scheduling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class ProcessorSchedulingProgram
+    {
+        private static int _count;
+
+        public static void Main()
+        {
+            var tasks = ReadTasks()
+                .OrderByDescending(x => x.Value)
+                .ToList();
+
+            var max = tasks.Select(x => x.Deadline)
+                .OrderByDescending(x => x).First();
+
+            var result = new List<Task>();
+
+            for (var i = 1; i <= max; i++)
+            {
+                var current = tasks
+                    .FirstOrDefault();
+
+                if (current == null)
+                {
+                    break;
+                }
+
+                tasks.Remove(current);
+                result.Add(current);
+                //tasks.RemoveAll(x => x.Deadline == i);
+            }
+
+            result = result.OrderBy(x => x.Deadline).ToList();
+            Console.WriteLine($"Optimal schedule: {string.Join(" -> ", result.Select(x => x.Number))}");
+            Console.WriteLine($"Total value: {result.Sum(x => x.Value)}");
+        }
+
+        private static List<Task> ReadTasks()
+        {
+            _count = int.Parse(Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                [1]);
+
+            var tasks = new List<Task>();
+
+            for (var i = 0; i < _count; i++)
+            {
+                var tokens = Console.ReadLine()
+                    .Split(new[] {" - "}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var value = tokens[0];
+                var deadline = tokens[1];
+                var number = i + 1;
+
+                var task = new Task(number, value, deadline);
+                tasks.Add(task);
+            }
+
+            return tasks;
+        }
+    }
+}
---
+++ 04. GREEDY ALGORITHMS/Exercise/02. Processor Scheduling/Task.cs
@@ -0,0 +1,23 @@
+﻿namespace _02._Processor_Scheduling
+{
+    public class Task
+    {
+        public Task(int number, int value, int deadline)
+        {
+            this.Number = number;
+            this.Value = value;
+            this.Deadline = deadline;
+        }
+
+        public int Number { get; }
+
+        public int Value { get; }
+
+        public int Deadline { get; }
+
+        public override string ToString()
+        {
+            return string.Format($"Number: {this.Number}, Value: {this.Value}, Deadline: {this.Deadline}");
+        }
+    }
+}
