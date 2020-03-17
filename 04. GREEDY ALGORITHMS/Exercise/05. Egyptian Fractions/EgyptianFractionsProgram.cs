3aec393e79b41fd2c224fbd6edf0e01a3f1b409c
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-17 16:07:52 +0100

    05. Egyptian Fractions

---
+++ 04. GREEDY ALGORITHMS/Exercise/05. Egyptian Fractions/EgyptianFractionsProgram.cs
@@ -0,0 +1,83 @@
+﻿namespace _05._Egyptian_Fractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EgyptianFractionsProgram
+    {
+        public static void Main()
+        {
+            var tokens = Console.ReadLine()
+                .Split(new[] {"/"}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var numerator = tokens[0];
+            var denominator = tokens[1];
+
+            if (numerator >= denominator)
+            {
+                Console.WriteLine("Error (fraction is equal to or greater than 1)");
+                return;
+            }
+
+            var result = new List<Fraction>();
+
+            while (numerator != 0)
+            {
+                var newDenominator = (int)Math.Ceiling(denominator / (double)numerator);
+                var newFraction = new Fraction(1, newDenominator);
+                result.Add(newFraction);
+
+                numerator = numerator * newDenominator - denominator;
+                denominator = denominator * newDenominator;
+
+                var gcf = GreatestCommonFactor(numerator, denominator);
+
+                while (gcf > 1)
+                {
+                    numerator /= gcf;
+                    denominator /= gcf;
+
+                    gcf = GreatestCommonFactor(numerator, denominator);
+                }
+            }
+
+            Console.WriteLine($"{tokens[0]}/{tokens[1]} = {string.Join(" + ", result)}");
+        }
+
+        private static int GreatestCommonFactor(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+
+            int min;
+            int max;
+
+            if (Math.Abs(a) < Math.Abs(b))
+            {
+                min = a;
+                max = b;
+            }
+            else
+            {
+                min = b;
+                max = a;
+            }
+
+            for (var i = Math.Abs(min); i > 0; i--)
+            {
+                if (a % i == 0 && 
+                    b % i == 0)
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ 04. GREEDY ALGORITHMS/Exercise/05. Egyptian Fractions/Fraction.cs
@@ -0,0 +1,20 @@
+﻿namespace _05._Egyptian_Fractions
+{
+    public class Fraction
+    {
+        public Fraction(int numerator, int denominator)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+
+        public int Numerator { get; }
+
+        public int Denominator { get; }
+
+        public override string ToString()
+        {
+            return $"{this.Numerator}/{this.Denominator}";
+        }
+    }
+}
