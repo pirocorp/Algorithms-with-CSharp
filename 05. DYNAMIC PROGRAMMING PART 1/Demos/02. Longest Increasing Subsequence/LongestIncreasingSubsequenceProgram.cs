f742c99f8d8b5331a62064de5d50a5f9a3c56253
Zdravko Zdravkov <zdr0@yahoo.com>
2020-03-18 12:57:42 +0100

    02. Longest Increasing Sub sequence

---
+++ 05. DYNAMIC PROGRAMMING PART 1/Demos/02. Longest Increasing Subsequence/LongestIncreasingSubsequenceProgram.cs
@@ -0,0 +1,67 @@
+﻿namespace _02._Longest_Increasing_Subsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class LongestIncreasingSubsequenceProgram
+    {
+        public static void Main()
+        {
+            var numbers = new[] {3, 14, 5, 12, 15, 7, 8, 9, 11, 10, 1};
+
+            var lengths = new int[numbers.Length];
+            var previous = new int[numbers.Length];
+
+            var maxIssLength = 0;
+            var maxIssIndex = -1;
+
+            for (var current = 0; current < numbers.Length; current++)
+            {
+                var maxLength = 1;
+                var prevIndex = -1;
+                var currentNumber = numbers[current];
+
+                for (var prev = 0; prev < current; prev++)
+                {
+                    var prevNumber = numbers[prev];
+                    var prevSolutionLength = lengths[prev]; 
+
+                    if (currentNumber > prevNumber &&
+                        maxLength <= prevSolutionLength)
+                    {
+                        maxLength = prevSolutionLength + 1;
+                        prevIndex = prev;
+                    }
+                }
+
+                lengths[current] = maxLength;
+                previous[current] = prevIndex;
+
+                if (maxLength > maxIssLength)
+                {
+                    maxIssLength = maxLength;
+                    maxIssIndex = current;
+                }
+            }
+
+            //Console.WriteLine(string.Join(" ", lengths));
+            //Console.WriteLine(string.Join(" ", previous));
+            Console.WriteLine($"Max Increasing Subsequence Length Is: {maxIssLength}");
+
+            var index = maxIssIndex;
+
+            var result = new List<int>();
+
+            while (index != -1)
+            {
+                var current = numbers[index];
+                index = previous[index];
+
+                result.Add(current);
+            }
+
+            result.Reverse();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
