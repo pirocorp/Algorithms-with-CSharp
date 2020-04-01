fbb5885bde62e0542c4806fcca0497fd9c34c669
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-01 14:05:36 +0200

    DijkstraPriorityQueue

--- 08. ADVANCED GRAPH ALGORITHMS - PART I/Lab/DijkstraPriorityQueue/Node.cs
+++ 08. ADVANCED GRAPH ALGORITHMS - PART I/Lab/DijkstraPriorityQueue/Node.cs
@@ -1,23 +1,20 @@
-﻿namespace Dijkstra
+﻿using System;
+
+public class Node : IComparable<Node>
 {
-    using System;
-
-    public class Node : IComparable<Node>
+    // set default value for the distance equal to positive infinity
+    public Node(int id, double distance = double.PositiveInfinity)
     {
-        // set default value for the distance equal to positive infinity
-        public Node(int id, double distance = double.PositiveInfinity)
-        {
-            this.Id = id;
-            this.DistanceFromStart = distance;
-        }
+        this.Id = id;
+        this.DistanceFromStart = distance;
+    }
 
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public double DistanceFromStart { get; set; }
+    public double DistanceFromStart { get; set; }
 
-        public int CompareTo(Node other)
-        {
-            return this.DistanceFromStart.CompareTo(other.DistanceFromStart);
-        }
+    public int CompareTo(Node other)
+    {
+        return this.DistanceFromStart.CompareTo(other.DistanceFromStart);
     }
 }
