d4dfe344944ee8f27d66b7c8102938c53c4f28fa
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-10 18:15:27 +0200

    03. Lumber

---
+++ 11. SOLVING PRACTICAL PROBLEMS - PART I/Lab/03. Lumber/Log.cs
@@ -0,0 +1,52 @@
+﻿namespace _03._Lumber
+{
+    using System;
+
+    public class Log
+    {
+        private readonly int _ax;
+        private readonly int _ay;
+        private readonly int _bx;
+        private readonly int _by;
+
+        public Log(int id, int ax, int ay, int bx, int by)
+        {
+            this.Id = id;
+            this._ax = ax;
+            this._ay = ay;
+            this._bx = bx;
+            this._by = by;
+        }
+
+        public Log(int id, params int[] coordinates) 
+            : this(id, coordinates[0], coordinates[1], coordinates[2], coordinates[3])
+        { }
+
+        public int Id { get; }
+
+        public int X => this._ax;
+
+        public int Y => this._ay;
+
+        public int Width => Math.Abs(this._ax - this._bx);
+
+        public int Height => Math.Abs(this._ay - this._by);
+
+        public bool IntersectsWith(Log rect) =>
+            (rect.X <= this.X + this.Width) && (this.X <= rect.X + rect.Width) &&
+            (rect.Y >= this.Y - this.Height) && (this.Y >= rect.Y - rect.Height);
+
+        public bool Intersect(Log other)
+        {
+            var horizontal = this._ax <= other._bx && this._bx >= other._ax;
+            var vertical = this._ay >= other._by && this._by <= other._ay;
+
+            return horizontal && vertical;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Id}: {{({this.X}, {this.Y}) W: {this.Width}, H: {this.Height}}}";
+        }
+    }
+}
---
+++ 11. SOLVING PRACTICAL PROBLEMS - PART I/Lab/03. Lumber/LumberProgram.cs
@@ -0,0 +1,168 @@
+﻿namespace _03._Lumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class LumberProgram
+    {
+        private static List<Log> _logs;
+        private static int _queriesCount;
+
+        private static List<int>[] _graph;
+        private static bool[] _visited;
+        private static int[] _id;
+        private static int _count = 0;
+
+        private static void ReadInput()
+        {
+            var inputData = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var logsCount = inputData[0];
+            _queriesCount = inputData[1];
+
+            _logs = new List<Log>();
+            _graph = new List<int>[logsCount + 1];
+            _visited = new bool[logsCount + 1];
+            _id = new int[logsCount + 1];
+
+            for (var i = 1; i <= logsCount; i++)
+            {
+                var coordinates = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var newLog = new Log(i, coordinates);
+
+                _graph[i] = new List<int>();
+
+                foreach (var element in _logs)
+                {
+                    if (element.Intersect(newLog))
+                    {
+                        _graph[element.Id].Add(i);
+                        _graph[i].Add(element.Id);
+                    }
+                }
+
+                _logs.Add(newLog);
+            }
+        }
+
+        private static bool CheckForPath(Log from, Log to)
+        {
+            var result = false;
+            var visited = new HashSet<Log>();
+
+            var queue = new Queue<Log>();
+            queue.Enqueue(from);
+            visited.Add(from);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current == to)
+                {
+                    return true;
+                }
+
+                foreach (var rectAngle in _logs)
+                {
+                    if (!visited.Contains(rectAngle) && rectAngle.IntersectsWith(current))
+                    {
+                        queue.Enqueue(rectAngle);
+                        visited.Add(rectAngle);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static void SlowSolution()
+        {
+            for (var i = 0; i < _queriesCount; i++)
+            {
+                var inputData = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = _logs[inputData[0] - 1];
+                var to = _logs[inputData[1] - 1];
+
+                if (CheckForPath(@from, to))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+
+        private static void Dfs(int vertex)
+        {
+            _visited[vertex] = true;
+            _id[vertex] = _count;
+
+            foreach (var child in _graph[vertex])
+            {
+                if (!_visited[child])
+                {
+                    Dfs(child);
+                }
+            }
+        }
+
+        private static void MapConnectedComponents()
+        {
+            for (var vertex = 1; vertex < _graph.Length; vertex++)
+            {
+                if (!_visited[vertex])
+                {
+                    Dfs(vertex);
+                    _count++;
+                }
+            }
+        }
+
+        private static void ProcessQueries()
+        {
+            for (var i = 0; i < _queriesCount; i++)
+            {
+                var args = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = args[0];
+                var to = args[1];
+
+                if (_id[from] == _id[to])
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            ReadInput();
+            //SlowSolution();
+
+            MapConnectedComponents();
+            ProcessQueries();
+        }
+    }
+}
