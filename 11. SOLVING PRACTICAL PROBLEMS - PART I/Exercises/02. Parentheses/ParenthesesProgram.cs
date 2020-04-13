73c8ac873e85c8a21e621db161a66fd20fc9f715
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-13 14:20:58 +0200

    02. Parentheses

---
+++ 11. SOLVING PRACTICAL PROBLEMS - PART I/Exercises/02. Parentheses/ParenthesesProgram.cs
@@ -0,0 +1,39 @@
+﻿namespace _02._Parentheses
+{
+    using System;
+
+    public static class ParenthesesProgram
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            PrintParentheses(n, 0, 0, string.Empty);
+        }
+
+        private static void PrintParentheses(int count, int openedCount, int closedCount, string result)
+        {
+            if (openedCount == count && closedCount == count)
+            {
+                Console.WriteLine(result);
+                return;
+            }
+
+            if (openedCount == closedCount && openedCount < count)
+            {
+                PrintParentheses(count, openedCount + 1, closedCount, result + "(");
+            }
+            else
+            {
+                if (openedCount < count)
+                {
+                    PrintParentheses(count, openedCount + 1, closedCount, result + "(");
+                }
+
+                if (closedCount < openedCount)
+                {
+                    PrintParentheses(count, openedCount, closedCount + 1, result + ")");
+                }
+            }
+        }
+    }
+}
