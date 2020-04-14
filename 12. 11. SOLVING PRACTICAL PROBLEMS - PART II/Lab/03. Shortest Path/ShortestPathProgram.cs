ee777a3834669f00757bba4bc0dda587bb522e00
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-14 12:20:34 +0200

    03. Shortest Path

---
+++ 12. 11. SOLVING PRACTICAL PROBLEMS - PART II/Lab/03. Shortest Path/ShortestPathProgram.cs
@@ -0,0 +1,59 @@
+﻿namespace _03._Shortest_Path
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class ShortestPathProgram
+    {
+        private static readonly List<string> _set = new List<string>{ "L", "R", "S" };
+
+        private static void GenerateVariations(int count, List<string> variations, string currentVariation)
+        {
+            if (currentVariation.Length == count)
+            {
+                variations.Add(currentVariation);
+                return;
+            }
+
+            foreach (var element in _set)
+            {
+                GenerateVariations(count, variations, currentVariation + element);
+            }
+        }
+
+        public static void Main()
+        {
+            var inputString = Console.ReadLine();
+            
+            var count = inputString.Count(x => x == '*');
+
+            var variations = new List<string>();
+            GenerateVariations(count, variations, string.Empty);
+
+            var result = new List<string>();
+
+            for (var i = 0; i < variations.Count; i++)
+            {
+                var variation = variations[i];
+                var currentString = inputString.ToList();
+
+                var index = currentString.IndexOf('*');
+                var varIndex = 0;
+
+                while (index != -1)
+                {
+                    currentString[index] = variation[varIndex];
+                    index = currentString.IndexOf('*');
+                    varIndex++;
+                }
+
+                result.Add(new string(currentString.ToArray()));
+            }
+
+            result.Sort();
+            Console.WriteLine(result.Count);
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
