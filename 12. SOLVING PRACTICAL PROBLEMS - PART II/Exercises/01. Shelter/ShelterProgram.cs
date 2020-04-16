dba9a1c7f3761b94505a5a2642b787972faac45d
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-16 11:46:21 +0200

    01. Shelter

---
+++ 12. SOLVING PRACTICAL PROBLEMS - PART II/Exercises/01. Shelter/ShelterProgram.cs
@@ -0,0 +1,22 @@
+﻿namespace _01._Shelter
+{
+    using System;
+    using System.Linq;
+
+    public static class ShelterProgram
+    {
+        public static void Main()
+        {
+            var inputArgs = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var soldersCount = inputArgs[0];
+            var sheltersCount = inputArgs[1];
+            var shelterCapacity = inputArgs[2];
+
+
+        }
+    }
+}
