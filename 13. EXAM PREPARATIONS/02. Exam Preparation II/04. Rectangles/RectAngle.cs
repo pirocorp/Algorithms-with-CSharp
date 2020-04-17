d3cb8024e2037465c2f2cbeca605ff4a6c6e13f6
Zdravko Zdravkov <zdr0@yahoo.com>
2020-04-17 19:32:24 +0200

    04. Rectangles - works correctly but is expected another implementation

---
+++ 13. EXAM PREPARATIONS/02. Exam Preparation II/04. Rectangles/RectAngle.cs
@@ -0,0 +1,43 @@
+﻿namespace _04._Rectangles
+{
+    using System;
+
+    public class RectAngle
+    {
+        public RectAngle(string name, int x1, int y1, int x2, int y2)
+        {
+            this.Name = name;
+            this.X1 = x1;
+            this.Y1 = y1;
+            this.X2 = x2;
+            this.Y2 = y2;
+        }
+
+        public string Name { get; }
+
+        public int X1 { get; }
+
+        public int Y1 { get; }
+
+        public int X2 { get; }
+
+        public int Y2 { get; }
+
+        public int Size => Math.Abs(this.X1 - this.X2) * Math.Abs(this.Y1 - this.Y2);
+
+        public bool isNested(RectAngle other)
+        {
+            var notNested = (this.X1 > other.X1 ||
+                            this.X2 < other.X2 ||
+                            this.Y1 < other.Y1 ||
+                            this.Y2 > other.Y2);
+
+            return !notNested;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name}|({this.X1}, {this.Y1}), ({this.X2}, {this.Y2})";
+        }
+    }
+}
---
+++ 13. EXAM PREPARATIONS/02. Exam Preparation II/04. Rectangles/RectanglesProgram.cs
@@ -0,0 +1,136 @@
+﻿namespace _04._Rectangles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class RectanglesProgram
+    {
+        private static List<RectAngle> _rectAngles;
+
+        private static int[] _lns;
+        private static int[] _prev;
+
+        private static void ReadInput()
+        {
+            _rectAngles = new List<RectAngle>();
+
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "End")
+            {
+                var rectangleArgs = inputLine
+                    .Split(": ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                var name = rectangleArgs[0];
+                var x1 = int.Parse(rectangleArgs[1]);
+                var y1 = int.Parse(rectangleArgs[2]);
+                var x2 = int.Parse(rectangleArgs[3]);
+                var y2 = int.Parse(rectangleArgs[4]);
+
+                var rect = new RectAngle(name, x1, y1, x2, y2);
+                _rectAngles.Add(rect);
+
+                inputLine = Console.ReadLine();
+            }
+
+            _rectAngles = _rectAngles
+                .OrderByDescending(x => x.Size)
+                .ToList();
+        }
+
+        private static void GetLongestNestedSubsequenceDp()
+        {
+            _lns = new int[_rectAngles.Count];
+            _prev = Enumerable.Repeat(-1, _rectAngles.Count).ToArray();
+
+            for (var currentIndex = 0; currentIndex < _rectAngles.Count; currentIndex++)
+            {
+                var element = _rectAngles[currentIndex];
+
+                var maxLen = 1;
+                var prev = -1;
+
+                for (var prevIndex = 0; prevIndex < currentIndex; prevIndex++)
+                {
+                    var prevElement = _rectAngles[prevIndex];
+                    var prevSolutionLength = _lns[prevIndex];
+
+                    if (prevElement.isNested(element))
+                    {
+                        if (maxLen <= prevSolutionLength)
+                        {
+                            maxLen = prevSolutionLength + 1;
+                            prev = prevIndex;
+                        }
+
+                        //Alphabetical Order
+                        if (maxLen == prevSolutionLength + 1)
+                        {
+                            if (_rectAngles[prev].Name.CompareTo(_rectAngles[prevIndex].Name) > 0)
+                            {
+                                prev = prevIndex;
+                            }
+                        }
+                    }
+                }
+
+                _lns[currentIndex] = maxLen;
+                _prev[currentIndex] = prev;
+            }
+        }
+
+        private static List<string> ReconstructSolution(int i)
+        {
+            var result = new Stack<string>();
+
+            while (i != -1)
+            {
+                result.Push(_rectAngles[i].Name);
+                i = _prev[i];
+            }
+
+            return result.ToList();
+        }
+
+        private static void LongestNestedSubSequenceSolution()
+        {
+            ReadInput();
+
+            GetLongestNestedSubsequenceDp();
+
+            var max = _lns.Max();
+
+            var solutions = new List<List<string>>();
+
+            for (var i = 0; i < _lns.Length; i++)
+            {
+                if (_lns[i] == max)
+                {
+                    var solution = ReconstructSolution(i);
+                    solutions.Add(solution);
+                }
+            }
+
+            var result = solutions
+                .Select(x => string.Join(" < ", x))
+                .OrderBy(x => x)
+                .First();
+
+            Console.WriteLine(result);
+        }
+
+        public static void Main()
+        {
+            //LongestNestedSubSequenceSolution();
+            ReadInput();
+
+            var rect = _rectAngles.FirstOrDefault(x => x.Name == "rect45630");
+
+            foreach (var rectAngle in _rectAngles)
+            {
+                Console.WriteLine($"{rect.Name} {(rect.isNested(rectAngle) ? "Nested" : "Not Nested")} {rectAngle.Name}");
+            }
+        }
+    }
+}
