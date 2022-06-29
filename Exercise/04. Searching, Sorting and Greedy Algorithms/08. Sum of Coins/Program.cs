08e1f79d49b27e71f8f0303e7f1d5ce4be3333f9
Zdravko Zdravkov <pirocorp@abv.bg>
2022-06-29 14:19:59 +0300

    04. Searching, Sorting and Greedy Algorithms - Problems 8 and 9

---
+++ Exercise/04. Searching, Sorting and Greedy Algorithms/08. Sum of Coins/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _08._Sum_of_Coins;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Program
+{
+    private static int[] coinTypes = Array.Empty<int>();
+
+    private static int target = 0;
+
+    private static readonly Dictionary<int, int> coins = new Dictionary<int, int>();
+
+    public static void Main(string[] args)
+    {
+        ReadInput();
+
+        SumCoins();
+
+        PrintOutput();
+    }
+
+    private static void ReadInput()
+    {
+        coinTypes = (Console.ReadLine() ?? string.Empty)
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim())
+            .Select(int.Parse)
+            .OrderByDescending(x => x)
+            .ToArray();
+
+        target = int.Parse(Console.ReadLine() ?? "0");
+    }
+
+    private static void SumCoins()
+    {
+        foreach (var coin in coinTypes)
+        {
+            var tackedCoins = target / coin;
+            target %= coin;
+
+            if (tackedCoins > 0)
+            {
+                coins[coin] = tackedCoins;
+            }
+        }
+    }
+
+    private static void PrintOutput()
+    {
+        if (target != 0)
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+
+        var totalCoins = coins.Sum(x => x.Value);
+
+        Console.WriteLine($"Number of coins to take: {totalCoins}");
+
+        var result = new StringBuilder();
+
+        coins
+            .OrderByDescending(x => x.Key)
+            .ToList()
+            .ForEach(x => result.AppendLine($"{x.Value} coin(s) with value {x.Key}"));
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Exercise/04. Searching, Sorting and Greedy Algorithms/09. Set Cover/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace _09._Set_Cover;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Program
+{
+    private static HashSet<int> universe = new HashSet<int>();
+
+    private static List<int[]> sets = new List<int[]>();
+
+    private static readonly List<int[]> tackedSets = new List<int[]>();
+
+    public static void Main()
+    {
+        ReadInput();
+
+        TakeSets();
+
+        PrintOutput();
+    }
+
+    private static void ReadInput()
+    {
+        universe = (Console.ReadLine() ?? string.Empty)
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim())
+            .Select(int.Parse)
+            .ToHashSet();
+
+        var n = int.Parse(Console.ReadLine() ?? "0");
+
+        for (var i = 0; i < n; i++)
+        {
+            var currentSet = (Console.ReadLine() ?? string.Empty)
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .Select(int.Parse)
+                .ToArray();
+
+            sets.Add(currentSet);
+        }
+    }
+
+    private static void TakeSets()
+    {
+        while (universe.Count > 0)
+        {
+            sets = sets
+                .OrderByDescending(s => s.Intersect(universe).Count())
+                .ToList();
+
+            var currentSet = sets.First();
+
+            tackedSets.Add(currentSet);
+            sets.Remove(currentSet);
+
+            foreach (var element in currentSet)
+            {
+                universe.Remove(element);
+            }
+        }
+    }
+
+    private static void PrintOutput()
+    {
+        Console.WriteLine($"Sets to take ({tackedSets.Count}):");
+
+        foreach (var tackedSet in tackedSets)
+        {
+            Console.WriteLine(string.Join(", ", tackedSet));
+        }
+    }
+}
